38fc26be8a72293c8600a07f18aa59698f7223d8
Michel John <michel.john@hotmail.com>
2023-05-29 10:06:40 -0300

    config navigation types

--- src/routes/Stack.tsx
+++ src/routes/Stack.tsx
@@ -1,13 +1,15 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+import { RootStackParamList } from '../../@types/routes'
+
 import { Home } from '../screens/Home'
 import { Profile } from '../screens/Profile'
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export function StackNavigation() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,19 @@
 import { useNavigation } from '@react-navigation/native'
 import { YStack, Text, Button } from 'tamagui'
 
+import { NavigationProps } from '../../../@types/routes'
+
 export function Home() {
-  const { navigate } = useNavigation()
+  const navigation = useNavigation<NavigationProps>()
+
+  function handleGoToProfile() {
+    navigation.navigate('Profile', { userId: '123' })
+  }
 
   return (
-    <YStack margin={10}>
-      <Text color="$color">Home</Text>
-      <Button onPress={() => navigate('Profile')}>Go to Profile</Button>
+    <YStack p={'$3.5'} flex={1} bg='$dark9'>
+      <Text color="$dark0">Home</Text>
+      <Button color='$dark0' onPress={handleGoToProfile}>Go to Profile</Button>
     </YStack>
   )
 }
